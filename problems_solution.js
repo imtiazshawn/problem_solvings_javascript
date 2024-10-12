b89b90deaa9cf6b6644070dffc81159f7623e507
Imtiaz Shawn <101446861+imtiazshawn@users.noreply.github.com>
2024-10-12 15:57:21 +0600

    Update problems_solution.js

--- problems_solution.js
+++ problems_solution.js
@@ -140,7 +140,6 @@ const findingLargestNum = (arr) => {
     return largestNum;
 }
 
-
 const findingSecondLargest = (arr) => {
     const largestNum = findingLargestNum(arr);
     let secondLargestNum = 0;
@@ -156,5 +155,91 @@ const findingSecondLargest = (arr) => {
     return secondLargestNum;
 }
 
-
 console.log(findingSecondLargest([7, 11, 34, 21]));
+
+
+// 0009: Sum of Digits of a Number
+const getSumOfNum = (num) => {
+    const numArr = num.toString().split('');
+    let newNumArr = [];
+    for(let i = 0; i < numArr.length; i++) {
+        newNumArr.push(Number(numArr[i]));
+    }
+    const calcNum = newNumArr.reduce((acc, current) => acc + current);
+    return calcNum
+}
+
+console.log(getSumOfNum(1234));
+
+
+
+// 0010: Capitalize the First Letter of Each Word in a String
+const getCapitalize = (str) => {
+    const firstLetter = str.charAt(0).toUpperCase();
+    const restLetter = str.slice(1)
+    return firstLetter + restLetter;
+}
+
+const getCapitalizeStr = (str) => {
+    const strArr = str.split(' ');
+    let newArr = [];
+    
+    for(let i = 0; i < strArr.length; i++) {
+        newArr.push(getCapitalize(strArr[i]));
+    }
+    const capStr = newArr.join(' ')
+    return capStr;
+}
+
+console.log(getCapitalizeStr('javascript is awesome'));
+
+
+// 0011: Check if Two Strings are Anagrams
+// Way 01:
+const checkAnagrams = (str1, str2) => {
+    str1Arr = str1.split('');
+    str2Arr = str2.split('');
+    let result = true;
+    
+    for(let i = 0; i < str1Arr.length; i++) {
+        if(str2Arr.includes(str1Arr[i])) {
+            result = true;
+        } else {
+            result = false;
+            break;
+        }
+    }
+    
+    return result;
+}
+
+console.log(checkAnagrams('lipten', 'silent'))
+
+// Way 02:
+const checkAnagrams = (str1, str2) => {
+    sortedStr1 = str1.toLowerCase().split('').sort().join('');
+    sortedStr2 = str2.toLowerCase().split('').sort().join('');
+    
+    return sortedStr1 === sortedStr2;
+}
+
+console.log(checkAnagrams('listen', 'silent'))
+
+
+
+// 0012: Merge Two Sorted Arrays
+const mergeNSortArr = (arr1, arr2) => {
+    let newArr = [];
+    
+    for(let i = 0; i < arr1.length; i++) {
+        newArr.push(arr1[i]);
+    }
+    
+    for(let i = 0; i < arr2.length; i++) {
+        newArr.push(arr2[i])
+    }
+    
+    return newArr.sort();
+}
+
+console.log(mergeNSortArr([2, 1, 3, 4], [6, 5, 9, 8, 7]));
